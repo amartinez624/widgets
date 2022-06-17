c7935c606fa752ffcb4e986bd9c59856bf3ae817
Alex <alex@testingmom.com>
2022-06-17 00:16:30 -0400

    ready for deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const items = [
+    {
+        title: "Who is Alex Martinez?",
+        content: "Alex Martinez is a software engineer."
+    },
+    {
+        title: "Does Alex know React?",
+        content: "Yeah, you're looking at it right now."
+    },
+    {
+        title: "Could Alex think of another blurb here?",
+        content: "No he could not."
+    }
+];
+
+const options = [
+    {
+        label: "Red",
+        value: "red"
+    },
+    {
+        label: "Light Blue",
+        value: "blue"
+    },
+    {
+        label: "Dark Green",
+        value: "green"
+    }
+];
+
+export default () => {
+    const [selected, setSelected] = useState(options[0]);
+    return (
+        <div>
+            <Header />
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown
+                    label="Select a color"
+                    options={options}
+                    selected={selected}
+                    onSelectedChange={setSelected}
+                />
+            </Route>
+            <Route path="/translate">
+                <Translate />
+            </Route>
+        </div>
+    )
 }
-
-export default App;
